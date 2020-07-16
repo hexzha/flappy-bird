f6a48201d0e38601de3b4c9b7e0cd0180b8d831d
Hexiang Zhang <sam10.zhang@gmail.com>
2020-07-16 16:24:34 -0500

    game ends when hitting ground

--- game.js
+++ game.js
@@ -104,11 +104,18 @@ const bird = {
 		this.frame = this.frame % this.animation.length;
 	
 		if(state.current == state.getReady) {
-		
+			this.y = 150; // RESET POSITION OF THE BIRD AFTER GAME OVER
 		}
 		else {
 			this.speed += this.gravity;
 			this.y += this.speed;
+
+			if(this.y + this.h/2 >= cvs.height - fg.h) {
+				this.y = cvs.height - fg.h - this.h/2;
+				if(state.current == state.game) {
+					state.current = state.over;
+				}
+			}
 		}
 	}
 }
